6ea069b9a0084c94ee114b4d24e07d10cde4ac33
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-06-05 06:38:47 +0200

    Add an extension that exposes async errors
    XCTest doesn't handle errors well in async tests. When relying simply on `async throws`, Xcode displays the error on the method signature of the failing test, and only reports that “the operation couldn't be completed.”
    
    Using this extension exposes the real message of the error. It would prefer if it also indicated the actual line that threw, but that has not been solved yet. Let's hope that it is possible to put this functionality in XCTest, and that it is updated soon.

--- Tests/PostgresSourceTests/PublishingTests.swift
+++ Tests/PostgresSourceTests/PublishingTests.swift
@@ -3,6 +3,16 @@ import XCTest
 import Postgres
 import Source
 
+extension XCTestCase {
+    public func exposeError(_ block: () async throws -> Void, file: StaticString = #file, line: UInt = #line) async {
+        do {
+            try await block()
+        } catch {
+            XCTFail("\(error)", file: file, line: line)
+        }
+    }
+}
+
 final class PublishingTests: XCTestCase {
     var publisher: EventPublisher!
     var entityStore: EntityStore!
@@ -13,94 +23,108 @@ final class PublishingTests: XCTestCase {
         publisher = EventPublisher(repository: database)
         entityStore = EntityStore(repository: database)
     }
-    
-    func test_canPublishEntityWithoutEvents() async throws {
-        let entity = Entity(id: "test", state: TestEntity())
-        entity.state.unpublishedEvents = []
 
-        let history = try await history(afterPublishingChangesFor: entity, actor: "user_x")
+    func test_canPublishEntityWithoutEvents() async {
+        await exposeError {
+            let entity = Entity(id: "test", state: TestEntity())
+            entity.state.unpublishedEvents = []
 
-        XCTAssertEqual(history?.type, TestEntity.typeId)
-        XCTAssertEqual(history?.id, "test")
-        XCTAssertEqual(history?.version, 0)
+            let history = try await history(afterPublishingChangesFor: entity, actor: "user_x")
+
+            XCTAssertEqual(history?.type, TestEntity.typeId)
+            XCTAssertEqual(history?.id, "test")
+            XCTAssertEqual(history?.version, 0)
+        }
     }
 
-    func test_canPublishSingleEvent() async throws {
-        let entity = Entity(id: "test", state: TestEntity())
-        entity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
+    func test_canPublishSingleEvent() async {
+        await exposeError {
+            let entity = Entity(id: "test", state: TestEntity())
+            entity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
 
-        let history = try await history(afterPublishingChangesFor: entity, actor: "user_x")
-        let event = history?.events.first
+            let history = try await history(afterPublishingChangesFor: entity, actor: "user_x")
+            let event = history?.events.first
 
-        XCTAssertEqual(event?.name, "AnEvent")
-        XCTAssertEqual(event?.jsonDetails, "{}")
-        XCTAssertEqual(event?.actor, "user_x")
+            XCTAssertEqual(event?.name, "AnEvent")
+            XCTAssertEqual(event?.jsonDetails, "{}")
+            XCTAssertEqual(event?.actor, "user_x")
+        }
     }
 
-    func test_versionMatchesNumberOfEvents() async throws {
-        let entity = Entity(id: "test", state: TestEntity())
-        entity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
+    func test_versionMatchesNumberOfEvents() async {
+        await exposeError {
+            let entity = Entity(id: "test", state: TestEntity())
+            entity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
 
-        let history = try await history(afterPublishingChangesFor: entity, actor: "user_x")
+            let history = try await history(afterPublishingChangesFor: entity, actor: "user_x")
 
-        XCTAssertEqual(history?.version, 1)
+            XCTAssertEqual(history?.version, 1)
+        }
     }
 
-    func test_canPublishMultipleEvents() async throws {
-        let entity = Entity(id: "test", state: TestEntity())
-        entity.state.unpublishedEvents = [
-            UnpublishedEvent(name: "AnEvent", details: "{}")!,
-            UnpublishedEvent(name: "AnEvent", details: "{}")!,
-            UnpublishedEvent(name: "AnEvent", details: "{}")!
-        ]
+    func test_canPublishMultipleEvents() async {
+        await exposeError {
+            let entity = Entity(id: "test", state: TestEntity())
+            entity.state.unpublishedEvents = [
+                UnpublishedEvent(name: "AnEvent", details: "{}")!,
+                UnpublishedEvent(name: "AnEvent", details: "{}")!,
+                UnpublishedEvent(name: "AnEvent", details: "{}")!
+            ]
 
-        let history = try await history(afterPublishingChangesFor: entity, actor: "any")
+            let history = try await history(afterPublishingChangesFor: entity, actor: "any")
 
-        XCTAssertEqual(history?.events.count, 3)
-        XCTAssertEqual(history?.version, 3)
+            XCTAssertEqual(history?.events.count, 3)
+            XCTAssertEqual(history?.version, 3)
+        }
     }
 
-    func test_addsEventsExistingEntity() async throws {
-        let existingEntity = Entity(id: "test", state: TestEntity())
-        try await publisher.publishChanges(entity: existingEntity, actor: "any")
+    func test_addsEventsExistingEntity() async {
+        await exposeError {
+            let existingEntity = Entity(id: "test", state: TestEntity())
+            try await publisher.publishChanges(entity: existingEntity, actor: "any")
 
-        let reconstitutedVersion = Entity(id: "test", state: TestEntity(), version: 0)
-        reconstitutedVersion.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
+            let reconstitutedVersion = Entity(id: "test", state: TestEntity(), version: 0)
+            reconstitutedVersion.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
 
-        let history = try await history(afterPublishingChangesFor: reconstitutedVersion, actor: "any")
+            let history = try await history(afterPublishingChangesFor: reconstitutedVersion, actor: "any")
 
-        XCTAssertEqual(history?.events.count, 1)
+            XCTAssertEqual(history?.events.count, 1)
+        }
     }
 
-    func test_throwsIfVersionHasChanged() async throws {
-        let existingEntity = Entity(id: "test", state: TestEntity())
-        existingEntity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
-        try await publisher.publishChanges(entity: existingEntity, actor: "any")
+    func test_throwsIfVersionHasChanged() async {
+        await exposeError {
+            let existingEntity = Entity(id: "test", state: TestEntity())
+            existingEntity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
+            try await publisher.publishChanges(entity: existingEntity, actor: "any")
 
-        let reconstitutedVersion = Entity(id: "test", state: TestEntity(), version: 0)
-        reconstitutedVersion.state.unpublishedEvents.append(UnpublishedEvent(name: "AnEvent", details: "{}")!)
+            let reconstitutedVersion = Entity(id: "test", state: TestEntity(), version: 0)
+            reconstitutedVersion.state.unpublishedEvents.append(UnpublishedEvent(name: "AnEvent", details: "{}")!)
 
-        await _XCTAssertThrowsError(try await publisher.publishChanges(entity: reconstitutedVersion, actor: "user_x"))
+            await _XCTAssertThrowsError(try await publisher.publishChanges(entity: reconstitutedVersion, actor: "user_x"))
+        }
     }
 
-    func test_updatesNextPosition() async throws {
-        let existingEntity = Entity(id: "test", state: TestEntity())
-        existingEntity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
-        try await publisher.publishChanges(entity: existingEntity, actor: "any")
+    func test_updatesNextPosition() async {
+        await exposeError {
+            let existingEntity = Entity(id: "test", state: TestEntity())
+            existingEntity.state.unpublishedEvents = [UnpublishedEvent(name: "AnEvent", details: "{}")!]
+            try await publisher.publishChanges(entity: existingEntity, actor: "any")
 
-        let entity = Entity(id: "test", state: TestEntity(), version: 1)
-        entity.state.unpublishedEvents = [
-            UnpublishedEvent(name: "AnEvent", details: "{}")!,
-            UnpublishedEvent(name: "AnEvent", details: "{}")!,
-            UnpublishedEvent(name: "AnEvent", details: "{}")!
-        ]
+            let entity = Entity(id: "test", state: TestEntity(), version: 1)
+            entity.state.unpublishedEvents = [
+                UnpublishedEvent(name: "AnEvent", details: "{}")!,
+                UnpublishedEvent(name: "AnEvent", details: "{}")!,
+                UnpublishedEvent(name: "AnEvent", details: "{}")!
+            ]
 
-        try await publisher.publishChanges(entity: entity, actor: "user_x")
+            try await publisher.publishChanges(entity: entity, actor: "user_x")
 
-        let nextPosition = try await database.nextPosition()
-        let maxPositionOfEvents = try await maxPositionOfEvents(forEntityWithId: "test")
-        XCTAssertEqual(nextPosition, 4)
-        XCTAssertEqual(maxPositionOfEvents, 3)
+            let nextPosition = try await database.nextPosition()
+            let maxPositionOfEvents = try await maxPositionOfEvents(forEntityWithId: "test")
+            XCTAssertEqual(nextPosition, 4)
+            XCTAssertEqual(maxPositionOfEvents, 3)
+        }
     }
 
     private func history<__State>(afterPublishingChangesFor entity: Entity<__State>, actor: String) async throws -> History? where __State: EntityState {
